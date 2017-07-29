ee4e0fc81dd7bbd2cb25ca2eecdc84d1ae9fecc0
andangiuli <30536005+andangiuli@users.noreply.github.com>
2017-07-29 19:14:30 +0200

    added conditions for boundaries

--- pi.py
+++ pi.py
@@ -5,20 +5,22 @@ import numpy
 
 def pi(x,x_min,x_max,delta_x):
 
-
     low=(x-x_min)//delta_x
 
-    if (x-x_min-low*delta_x)<(x_min+(low+1)*delta_x-x):
+    if low>num_t-1:
+
+        x_index=num_x-1
+
+    elif low<1:
+
+        x_index=0
+
+    elif (x-x_min-low*delta_x)<(x_min+(low+1)*delta_x-x):
 
         x_index=low
-
     else:
 
         x_index=low+1
-
-    # print(x_index)
-    # print(x-x_min-low*delta_x)
-    # print(x_min+(low+1)*delta_x-x)
 
     return(x_index)
 
