ea09e5aae8b198197e0f13b76b074c3124d6cd7c
li houzhi <houzhi.li@polytechnique.edu>
2017-07-31 17:10:00 +0200

    Update main_grid.py

--- main_grid.py
+++ main_grid.py
@@ -88,30 +88,29 @@ def backward(mu,u_old,v_old):
     
     u = np.zeros((num_t,num_x))
     v = np.zeros((num_t,num_x))
-    
-    for j in range(num_x):
-        u[num_t-1][j] = y_grid[pi(g(x_grid[num_t-1][j]),y_min,y_max,delta_y)]
-        v[num_t-1][j] = v_old[num_t-1][j]
-    
+        
+    u[num_t-1,:] = g(x_grid)
+    v[num_t-1,:] = v_old[num_t-1,:]
 
     for i in reversed(range(num_t-1)):
         for j in range(num_x):
             
-            j_down = pi((x_grid[j] + b(i, j, mu, u_old, v_old) * delta_t - sigma * sqrt(delta_t)), x_min, x_max, delta_x)
+            j_down = pi(x_grid[j] + b(i, j, mu, u_old, v_old) * delta_t - sigma * np.sqrt(delta_t))
 
-#            j_down = pi((x_grid[j] + b(i+1,j,mu,u,v)*delta_t - sigma*sqrt(delta_t)), x_min,x_max,delta_x)
+#            j_down = pi((x_grid[j] + b(i+1,j,mu,u,v)*delta_t - sigma*np.sqrt(delta_t)), x_min,x_max,delta_x)
             
-            j_up = pi((x_grid[j] + b(i, j, mu, u_old, v_old) * delta_t + sigma * sqrt(delta_t)), x_min, x_max, delta_x)
+            j_up = pi(x_grid[j] + b(i, j, mu, u_old, v_old) * delta_t + sigma * np.sqrt(delta_t))
 
-#            j_up = pi((x_grid[j] + b(i+1,j,mu,u,v)*delta_t + sigma*sqrt(delta_t)), x_min,x_max,delta_x)
+#            j_up = pi((x_grid[j] + b(i+1,j,mu,u,v)*delta_t + sigma*np.sqrt(delta_t)), x_min,x_max,delta_x)
             
             u[i][j] = (u[i+1][j_down] + u[i+1][j_up])/2.0 + delta_t*f(i,j,mu,u_old,v_old)
            
 #            u[i][j] = y_grid[pi(((u[i+1][j_down] + delta_t*f(i+1,j_down,mu,u,v) + u[i+1][j_up] + delta_t*f(i+1,j_up,mu,u,v))/2.0), y_min, y_max, delta_y)]
             
-            v[i][j] = 1.0/sqrt(delta_t) * (u[i+1][j_up] - u[i+1][j_down])
+            v[i][j] = 1.0/np.sqrt(delta_t) * (u[i+1][j_up] - u[i+1][j_down])
 
     return [u,v]
+
 
 if __name__ == '__main__':
     global b
@@ -143,6 +142,16 @@ if __name__ == '__main__':
     global x_grid
     x_grid=np.linspace(x_min,x_max,num_x)
     
+    global y_min
+    y_min=-5
+    global y_max
+    y_max=5
+    global num_y
+    num_y=10
+    global delta_y
+    delta_y=float(y_max-y_min)/(num_y-1)
+    global y_grid
+    y_grid=np.linspace(y_min,y_max,num_y)
 
     global sigma
     sigma=1
