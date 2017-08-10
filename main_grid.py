84007eadb0d2b31dbe092ee8ad8b0a3ede47b815
Christy Graves <cjvaughn@princeton.edu>
2017-08-10 14:35:29 +0200

    indentation fixes

--- main_grid.py
+++ main_grid.py
@@ -981,28 +981,26 @@ if __name__ == '__main__':
         mu=forward(u,v,mu_0)
 
         np.save('solution_trader.npy',mu)
-    elif execution=='true_start': # problem has to be 'trader_Pontryagin'
+    elif execution=='true_start': # only for certain problems
         if periodic_2_pi:
             mu=np.load('mu_reference_set_158.npy')*delta_x
             u=np.load('u_reference_set_158.npy')
         elif problem=='trader_weak' or problem=='trader_Pontryagin':
             mu=true_solution=np.load('solution_trader.npy')
-            mu_0=mu[0]
-            u=np.zeros((num_t,num_x))
-            v=np.zeros((num_t,num_x))
-            index2=0
-            all_Y_0_values=np.zeros((1,num_keep))
-            for j in range(J):
-                [u,v]=backward(mu,u,v)
-                mu=forward(u,v,mu_0)
-                if j>J-num_keep-1:
-                    all_Y_0_values[0][index2]=np.dot(u[0],mu[0])
-                    index2+=1
-            print all_Y_0_values[0]
+        mu_0=mu[0]
+        u=np.zeros((num_t,num_x))
+        v=np.zeros((num_t,num_x))
+        index2=0
+        all_Y_0_values=np.zeros((1,num_keep))
+        for j in range(J):
+            [u,v]=backward(mu,u,v)
+            mu=forward(u,v,mu_0)
+            if j>J-num_keep-1:
+                all_Y_0_values[0][index2]=np.dot(u[0],mu[0])
+                index2+=1
+        print all_Y_0_values[0]
 
-
-
-            np.save('mu_trader_true_start_t20.npy',mu)
+        #np.save('mu_trader_true_start_t20.npy',mu)
             
             
     elif execution=='continuation_in_time':
